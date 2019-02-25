a414a240ec5caa68e9a0db51e1b348e261535eaf
baozhoutao <baozhoutao@hotoa.com>
2019-02-25 16:50:34 +0800

    fix bug

--- main.js
+++ main.js
@@ -1,43 +1,44 @@
 var fs = require("fs");
 var path = require("path");
+// var chokidar = require('chokidar')
 
-var appsDirectory = path.join(__dirname, "apps");
 var appFileName = 'app.json'
 var appObjectFolderName = 'objects'
-if(fs.existsSync(appsDirectory)){
-    fs.readdir(appsDirectory, function(err, files){
-       _.each(files, function(f){
-        var curPath = path.join(appsDirectory, f);
-            if(fs.statSync(curPath).isDirectory()){
-                fs.readdir(curPath, function(err, appFiles){
-                    var appFilePath = path.join(curPath, appFileName)
-                    if(fs.existsSync(appFilePath)){
-                        var app = JSON.parse(fs.readFileSync(appFilePath, 'utf8').normalize('NFC'))
-                        Creator.Apps[app.name] = app
+var appSrcDirectory = path.join(__dirname, "src");
 
-                        fs.watchFile(appFilePath, (curr, prev) => {
-                            var app = JSON.parse(fs.readFileSync(appFilePath, 'utf8').normalize('NFC'))
-                            Creator.Apps[app.name] = app
-                        });
-                    }
-                    var appObjectFolderPath = path.join(curPath, appObjectFolderName)
-                    if(fs.existsSync(appObjectFolderPath)){
-                        fs.readdir(appObjectFolderPath, function(err, appObjectFiles){
-                            _.each(appObjectFiles, function(aof){
-                                var objectPath = path.join(appObjectFolderPath, aof)
-                                var appObj = JSON.parse(fs.readFileSync(objectPath, 'utf8').normalize('NFC'))
-                                Creator.Objects[appObj.name] = appObj
-                                Creator.loadObjects(appObj)
-                                fs.watchFile(objectPath, (curr, prev) => {
-                                    var appObj = JSON.parse(fs.readFileSync(objectPath, 'utf8').normalize('NFC'))
-                                    Creator.Objects[appObj.name] = appObj
-                                    //Creator.loadObjects(appObj)
-                                });
-                            })
-                        })
-                    }
+if(fs.statSync(appSrcDirectory).isDirectory()){
+    fs.readdir(appSrcDirectory, function(err, appFiles){
+        var appFilePath = path.join(appSrcDirectory, appFileName)
+        if(fs.existsSync(appFilePath)){
+            var app = JSON.parse(fs.readFileSync(appFilePath, 'utf8').normalize('NFC'))
+            Creator.Apps[app.name] = app
+
+            fs.watchFile(appFilePath, (curr, prev) => {
+                var app = JSON.parse(fs.readFileSync(appFilePath, 'utf8').normalize('NFC'))
+                Creator.Apps[app.name] = app
+            });
+        }
+        var appObjectFolderPath = path.join(appSrcDirectory, appObjectFolderName)
+        if(fs.existsSync(appObjectFolderPath)){
+            fs.readdir(appObjectFolderPath, function(err, appObjectFiles){
+                _.each(appObjectFiles, function(aof){
+                    var objectPath = path.join(appObjectFolderPath, aof)
+                    console.log('objectPath', objectPath);
+                    var appObj = JSON.parse(fs.readFileSync(objectPath, 'utf8').normalize('NFC'))
+                    Creator.Objects[appObj.name] = appObj
+                    Creator.loadObjects(appObj)
+                    fs.watchFile(objectPath, (curr, prev) => {
+                        var appObj = JSON.parse(fs.readFileSync(objectPath, 'utf8').normalize('NFC'))
+                        Creator.Objects[appObj.name] = appObj
+                        Creator.fiberLoadObjects(appObj)
+                    });
                 })
-            }
-       })
+            })
+        }
+    })
+
+    //监控apps文件夹变化
+    fs.watch(appSrcDirectory, function(eventType, filename){
+        console.log('fs.watch', eventType, filename);
     })
 }
