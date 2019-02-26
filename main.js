8d985fa7c828f3f76f7d203e43d2cf66b53fee6e
baozhoutao <baozhoutao@hotoa.com>
2019-02-26 10:34:09 +0800

    支持加载yml格式文件

--- main.js
+++ main.js
@@ -1,21 +1,56 @@
 var fs = require("fs");
 var path = require("path");
-// var chokidar = require('chokidar')
+var yaml = require('js-yaml')
 
 var appFileName = 'app.json'
 var appObjectFolderName = 'objects'
 var appSrcDirectory = path.join(__dirname, "src");
 
+var loadJSONFile = function(filePath){
+    return JSON.parse(fs.readFileSync(filePath, 'utf8').normalize('NFC'));
+}
+
+var loadYmlFile = function(filePath){
+    var doc = yaml.safeLoad(fs.readFileSync(filePath, 'utf8'));
+    return doc
+}
+
+var loadFile = function(filePath){
+    console.info('loadFile', filePath);
+    try {
+        var extname = path.extname(filePath)
+        if(extname.toLocaleLowerCase() == '.json')
+            return loadJSONFile(filePath)
+        else if(extname.toLocaleLowerCase() == '.yml')
+            return loadYmlFile(filePath)
+    } catch (error) {
+        console.error('loadFile error', filePath, error)
+        return {}
+    }
+}
+
+var loadApp = function(appFilePath){
+    var app = loadFile(appFilePath)
+    if(!_.isEmpty(app)){
+        Creator.Apps[app.name] = app
+    }
+}
+
+var loadObject = function(objectPath){
+    var appObj =loadFile(objectPath)
+    if(!_.isEmpty(appObj)){
+        Creator.Objects[appObj.name] = appObj
+        Creator.fiberLoadObjects(appObj)
+    }
+}
+
 if(fs.statSync(appSrcDirectory).isDirectory()){
     fs.readdir(appSrcDirectory, function(err, appFiles){
         var appFilePath = path.join(appSrcDirectory, appFileName)
         if(fs.existsSync(appFilePath)){
-            var app = JSON.parse(fs.readFileSync(appFilePath, 'utf8').normalize('NFC'))
-            Creator.Apps[app.name] = app
-
+            loadApp(appFilePath)
             fs.watchFile(appFilePath, (curr, prev) => {
-                var app = JSON.parse(fs.readFileSync(appFilePath, 'utf8').normalize('NFC'))
-                Creator.Apps[app.name] = app
+                loadApp(appFilePath)
             });
         }
         var appObjectFolderPath = path.join(appSrcDirectory, appObjectFolderName)
@@ -23,14 +58,9 @@ if(fs.statSync(appSrcDirectory).isDirectory()){
             fs.readdir(appObjectFolderPath, function(err, appObjectFiles){
                 _.each(appObjectFiles, function(aof){
                     var objectPath = path.join(appObjectFolderPath, aof)
-                    console.log('objectPath', objectPath);
-                    var appObj = JSON.parse(fs.readFileSync(objectPath, 'utf8').normalize('NFC'))
-                    Creator.Objects[appObj.name] = appObj
-                    Creator.loadObjects(appObj)
+                    loadObject(objectPath)
                     fs.watchFile(objectPath, (curr, prev) => {
-                        var appObj = JSON.parse(fs.readFileSync(objectPath, 'utf8').normalize('NFC'))
-                        Creator.Objects[appObj.name] = appObj
-                        Creator.fiberLoadObjects(appObj)
+                        loadObject(objectPath)
                     });
                 })
             })
