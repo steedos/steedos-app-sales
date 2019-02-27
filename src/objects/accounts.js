802bb2ec46423349a53747dfaef6ae0225a1f8b7
Jack Zhuang <steedos@users.noreply.github.com>
2019-02-27 16:35:45 +0800

    扩展接口

---
+++ src/objects/accounts.js
@@ -0,0 +1,26 @@
+module.export = {
+    beforeInsert: function(){
+
+    },
+    beforeUpdate: function(){
+
+    },
+    beforeDelete: function(){
+
+    },
+    afterInsert: function(){
+
+    },
+    afterUpdate: function(){
+
+    },
+    afterDelete: function(){
+
+    },
+    afterUndelete: function(){
+
+    },
+    beforeSubmit: function (){
+
+    }
+}
